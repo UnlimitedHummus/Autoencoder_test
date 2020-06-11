88c1f6dd5fc46967f0fe13108252d81310fac45d
M46NA <35585143+M46NA@users.noreply.github.com>
2020-06-11 16:35:13 +0200

    added comments

--- model_serving.py
+++ model_serving.py
@@ -3,10 +3,9 @@ import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 
-(X_train, _), (X_test, _) = mnist.load_data()
+(X_train, _), (X_test, _) = mnist.load_data()  # loading dataset
 
-# normalize
-X_test, X_train = X_test/255.0, X_train/255.0
+X_test, X_train = X_test/255.0, X_train/255.0  # normalizing data
 
 autoencoder = keras.models.load_model("autoencoder")  # loading the model from memory
 
@@ -16,10 +15,10 @@ def plot_image(image):
     plt.axis("off")
 
 
-def show_reconstructions(model, n_images=5):
-    reconstructions = model.predict(X_test[:n_images])
+def show_reconstructions(model, n_images=5):  # showing images before and after autoencoding
+    reconstructions = model.predict(X_test[:n_images])  # sending images through autoencoder
     fig = plt.figure(figsize=(n_images * 1.5, 3))
-    for image_index in range(n_images):
+    for image_index in range(n_images):  # plotting images vs autoencoded images
         plt.subplot(2, n_images, 1 + image_index)
         plot_image(X_test[image_index])
         plt.subplot(2, n_images, 1 + n_images + image_index)
--- model_training.py
+++ model_training.py
@@ -2,33 +2,32 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.datasets import mnist
 
-print(tf.test.gpu_device_name())
+# print(tf.test.gpu_device_name())
 
-(X_train, _), (X_test, _) = mnist.load_data()
+(X_train, _), (X_test, _) = mnist.load_data()  # loading the mnist dataset
 
-# normalize
-X_test, X_train = X_test/255.0, X_train/255.0
+X_test, X_train = X_test/255.0, X_train/255.0  # normalizing the data
 
 
-stacked_encoder = keras.models.Sequential([
+stacked_encoder = keras.models.Sequential([  # building the encoder
 
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(100, activation="selu"),
     keras.layers.Dense(30, activation="selu")
 ])
 
-stacked_decoder = keras.models.Sequential([
+stacked_decoder = keras.models.Sequential([  # building the decoder
     keras.layers.Dense(100, activation="selu", input_shape=[30]),
     keras.layers.Dense(28 * 28, activation="sigmoid"),
     keras.layers.Reshape([28, 28])
 ])
 
-stacked_autoencoder = keras.models.Sequential([stacked_encoder, stacked_decoder])
-stacked_autoencoder.summary()
+stacked_autoencoder = keras.models.Sequential([stacked_encoder, stacked_decoder])  # putting autoencoder together
+stacked_autoencoder.summary()  # printing summary of model
 stacked_autoencoder.compile(loss="binary_crossentropy",
-                            optimizer=keras.optimizers.SGD(lr=1.5, momentum=0.9))
+                            optimizer=keras.optimizers.SGD(lr=1.5, momentum=0.9))  # compiling model
 
 history = stacked_autoencoder.fit(X_train, X_train, epochs=30,
-                                  validation_data=(X_test, X_test))
+                                  validation_data=(X_test, X_test))  # training model
 
 stacked_autoencoder.save('autoencoder')  # saving autoencoder so we don't have to train it every time
